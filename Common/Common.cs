e00eb33dabf700dbcae2aa03d290ed92042bd1e1
BOREN FU <zxc37880@gmail.com>
2019-12-18 17:58:07 +0800

    Add RunProcess方法，執行外部程式。例如.bat檔。

--- Common/Common.cs
+++ Common/Common.cs
@@ -7,7 +7,7 @@
 // is obtained.
 // </copyright>
 // <date>       Created : 2019/12/16   </date>
-// <brief>      Description :  
+// <brief>      Description :
 // </brief>
 // <author>     BoRen
 // </author>
@@ -15,14 +15,9 @@
 namespace AlCommon.Util
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Runtime.Serialization.Formatters.Binary;
-    using System.Reflection;
-    using Microsoft.Win32;
+    using System.Diagnostics;
     using System.IO;
+    using System.Runtime.Serialization.Formatters.Binary;
 
     /// <summary>
     /// 一般常用的函式放在這裏
@@ -30,6 +25,7 @@ namespace AlCommon.Util
     public class Common
     {
         #region Private Member
+
         /// <summary>
         /// 讓程式自動退出的機制
         /// </summary>
@@ -39,9 +35,11 @@ namespace AlCommon.Util
         /// 是否結束，此flag特別用在Wait()，若是無限等待的話，Wait()會進入後退不出來，特別用此flag通知
         /// </summary>
         private static bool _isExit { get; set; }
-        #endregion
+
+        #endregion Private Member
 
         #region Inner class
+
         /// <summary>
         /// 用來設定Common._isExit = true, 確保所有的wait(無限等待)的情況可以避免，否則AP程式會無法結束
         /// </summary>
@@ -62,9 +60,11 @@ namespace AlCommon.Util
                 Common._isExit = true;
             }
         }
-        #endregion
+
+        #endregion Inner class
 
         #region Constant
+
         /// <summary>
         /// For Wait()
         /// </summary>
@@ -74,7 +74,26 @@ namespace AlCommon.Util
         /// Wait() Function loop wait time
         /// </summary>
         private const Int32 MAX_WAIT = 100;
-        #endregion
+
+        #endregion Constant
+
+        /// <summary>
+        /// 深層複製
+        /// </summary>
+        /// <typeparam name="T">複製型態</typeparam>
+        /// <param name="obj">要複製的物件</param>
+        /// <returns>回傳複製的物件</returns>
+        public static T DeepClone<T>(T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                ms.Position = 0;
+
+                return (T)formatter.Deserialize(ms);
+            }
+        }
 
         /// <summary>
         /// 無條件捨去
@@ -96,24 +115,6 @@ namespace AlCommon.Util
         }
 
         /// <summary>
-        /// 深層複製
-        /// </summary>
-        /// <typeparam name="T">複製型態</typeparam>
-        /// <param name="obj">要複製的物件</param>
-        /// <returns>回傳複製的物件</returns>
-        public static T DeepClone<T>(T obj)
-        {
-            using(var ms = new MemoryStream())
-            {
-                var formatter = new BinaryFormatter();
-                formatter.Serialize(ms, obj);
-                ms.Position = 0;
-
-                return (T)formatter.Deserialize(ms);
-            }
-        }
-
-        /// <summary>
         /// 把value的精度，轉成和basevalue多一位(小數點位數多一位)
         /// </summary>
         /// <param name="value">欲齊次的值</param>
@@ -132,6 +133,71 @@ namespace AlCommon.Util
 
             double newValue = Math.Round(value, decimals + 1);
             return newValue;
+        }
+
+        /// <summary>
+        /// 執行外部程式
+        /// </summary>
+        /// <param name="executeFile">執行檔檔名</param>
+        /// <param name="parameters">參數</param>
+        /// <param name="showWindow">探否有視窗</param>
+        /// <returns>成功執行的話，傳回proc.Id, 否則為-1</returns>
+        public static int RunProcess(string executeFile, string paameter = "", bool showWindow = false)
+        {
+            //create a process info object so we can run our app
+            ProcessStartInfo oInfo = new ProcessStartInfo(executeFile, paameter);
+            oInfo.UseShellExecute = false;
+            oInfo.CreateNoWindow = !showWindow;
+            oInfo.RedirectStandardOutput = true;
+
+            Process proc = null;
+            try
+            {
+                //run the process
+                proc = Process.Start(oInfo);
+                string result = proc.StandardOutput.ReadToEnd();
+                proc.WaitForExit();
+                return proc.Id;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        /// <summary>
+        /// 執行外部程式
+        /// </summary>
+        /// <param name="executeFile">執行檔檔名</param>
+        /// <param name="parameters">參數</param>
+        /// <param name="workDir">工作目錄</param>
+        /// <param name="result">輸出結果</param>
+        /// <returns>成功執行的話，傳回True, 否則為False</returns>
+        public static bool RunProcess(string executeFile, string parameters, string workDir, ref string result)
+        {
+            try
+            {
+                var info = new ProcessStartInfo(executeFile, parameters)
+                {
+                    CreateNoWindow = true,
+                    RedirectStandardInput = false,
+                    RedirectStandardError = false,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    WorkingDirectory = Path.GetDirectoryName(workDir),
+                };
+
+                using (var process = Process.Start(info))
+                {
+                    result = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
